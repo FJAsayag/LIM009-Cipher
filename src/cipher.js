3f2c7d996b0888f4569e7e7e5f739f2dae3bdb2e
FJAsayag <Asayag2018@hotmail.com>
2019-02-23 16:40:42 -0500

    adding content to encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,50 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (num, str) => {
+    num %= 26;
+    let asciiCode = 0;
+    let newStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+      
+      asciiCode = str.charCodeAt(i);
+
+      const toText = () => {
+        asciiCode = String.fromCharCode(asciiCode);
+      }  
+
+      if (asciiCode>= 65 && asciiCode<=90) {
+        asciiCode += num;
+
+        if(asciiCode>90){
+            asciiCode -= 26;
+        }
+    
+        toText();
+        
+      } else if (asciiCode>=97 && asciiCode<=122){
+        asciiCode += num;
+
+        if(asciiCode>122){
+            asciiCode -= 26;
+        }
+
+        toText();
+       
+      } else {
+        asciiCode;
+
+        toText();
+      }    
+     
+      
+    newStr += asciiCode;
+    }
+
+    console.log(newStr);
+   return newStr;
   },
+
+
   decode: () => {
     /* Acá va tu código */
   }
