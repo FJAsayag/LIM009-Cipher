e7d9004e7d3e3eaa0b2eb34c0676872f0e273a72
FJAsayag <Asayag2018@hotmail.com>
2019-02-23 17:56:40 -0500

    adding decoding code, button and function to decoding button

--- src/cipher.js
+++ src/cipher.js
@@ -45,7 +45,48 @@ window.cipher = {
   },
 
 
-  decode: () => {
-    /* Acá va tu código */
+  decode: (num, str) => {
+    num %= 26;
+    let asciiCode = 0;
+    let newStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+      
+      asciiCode = str.charCodeAt(i);
+
+      const toText = () => {
+        asciiCode = String.fromCharCode(asciiCode);
+      }  
+
+      if (asciiCode>= 65 && asciiCode<=90) {
+        asciiCode -= num;
+      
+        if(asciiCode<65){
+            asciiCode += 26;
+        }
+      
+        toText();
+        
+      } else if (asciiCode>=97 && asciiCode<=122){
+        asciiCode -= num;
+      
+        if(asciiCode<97){
+            asciiCode += 26;
+        }
+      
+        toText();
+       
+      } else {
+        asciiCode;
+      
+        toText();
+      }    
+     
+      
+    newStr += asciiCode;
+    }
+
+    console.log(newStr);
+   return newStr;
   }
-};
+}
--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ document.getElementById("encoding").addEventListener("click", () => {
 
   console.log(encodedText)
 });
+
+document.getElementById("decoding").addEventListener("click", () => {
+  //here goes the DOM that plays with the views.
+  
+  const writtenText = document.getElementById("written-text").value;
+  let offset = parseInt(document.getElementById("offset").value);
+  let decodedText = "";
+
+  decodedText = cipher.decode(offset, writtenText);
+
+  console.log(decodedText)
+});
