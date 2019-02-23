2ac074ca53f58300fa5c019f1503a309ffeaeb44
FJAsayag <Asayag2018@hotmail.com>
2019-02-23 16:43:18 -0500

    sending encoding code to cipher.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,11 @@
-document.getElementById("btn").addEventListener("click", () => {
+document.getElementById("encoding").addEventListener("click", () => {
+  //here goes the DOM that plays with the views.
+  
   const writtenText = document.getElementById("written-text").value;
   let offset = parseInt(document.getElementById("offset").value);
-      offset %= 26;
-  let asciiCode = 0;
-  let phrase = "";
+  let encodedText = "";
 
-  for (let i = 0; i < writtenText.length; i++) {
-      
-      asciiCode = writtenText.charCodeAt(i);
+  encodedText = cipher.encode(offset, writtenText);
 
-      const toText = () => {
-        asciiCode = String.fromCharCode(asciiCode);
-      }  
-
-      if (asciiCode>= 65 && asciiCode<=90) {
-        asciiCode += offset;
-        console.log(asciiCode)
-
-        if(asciiCode>90){
-            asciiCode -= 26;
-        }
-    
-        toText();
-        
-      } else if (asciiCode>=97 && asciiCode<=122){
-        asciiCode += offset;
-        console.log(asciiCode)
-
-        if(asciiCode>122){
-            asciiCode -= 26;
-        }
-
-        toText();
-       
-      } else {
-        asciiCode;
-        console.log(asciiCode)
-
-        toText();
-      }    
-     
-      
-    phrase += asciiCode;
-    console.log(phrase)
-    }
-
+  console.log(encodedText)
 });
