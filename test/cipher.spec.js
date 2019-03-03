a45de9bd695d00d8a93c157b8d3c99a59f228761
FJAsayag <Asayag2018@hotmail.com>
2019-03-02 22:27:22 -0500

    ; added

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -19,23 +19,23 @@ describe('cipher', () => {
     });
 
     it('debería retornar "456" para "123" con offset 33', () => {
-      assert.equal(cipher.encode(33, "123"), "456")
+      assert.equal(cipher.encode(33, "123"), "456");
     });
 
     it('debería retornar "&(" para "#%" con offset 33', () => {
-      assert.equal(cipher.encode(33, "#%"), "&(")
+      assert.equal(cipher.encode(33, "#%"), "&(");
     });
 
     it('debería retornar ":;<" para "<=>" con offset 33', () => {
-      assert.equal(cipher.encode(33, "<=>"), ":;<")
+      assert.equal(cipher.encode(33, "<=>"), ":;<");
     });
 
     it('debería retornar "^`" para "[]" con offset 33', () => {
-      assert.equal(cipher.encode(33, "[]"), "^`")
+      assert.equal(cipher.encode(33, "[]"), "^`");
     });
 
     it('debería retornar " " para " " con offset 33', () => {
-      assert.equal(cipher.encode(33, " "), " ")
+      assert.equal(cipher.encode(33, " "), " ");
     });
   });
 
@@ -46,31 +46,31 @@ describe('cipher', () => {
     });
 
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
-      assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+      assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     });
 
     it('debería retornar "abc" para "hij" con offset 33', () => {
-      assert.equal(cipher.decode(33, "hij"), "abc")
+      assert.equal(cipher.decode(33, "hij"), "abc");
     });
 
     it('debería retornar "123" para "456" con offset 33', () => {
-      assert.equal(cipher.decode(33, "456"), "123")
+      assert.equal(cipher.decode(33, "456"), "123");
     });
 
     it('debería retornar "#%" para "&(" con offset 33', () => {
-      assert.equal(cipher.decode(33, "&("), "#%")
+      assert.equal(cipher.decode(33, "&("), "#%");
     });
 
     it('debería retornar "<=>" para ":;<" con offset 33', () => {
-      assert.equal(cipher.decode(33, ":;<"), "<=>")
+      assert.equal(cipher.decode(33, ":;<"), "<=>");
     });
 
     it('debería retornar "[]" para "^`" con offset 33', () => {
-      assert.equal(cipher.decode(33, "^`"), "[]")
+      assert.equal(cipher.decode(33, "^`"), "[]");
     });
 
     it('debería retornar " " para " " con offset 33', () => {
-      assert.equal(cipher.decode(33, " "), " ")
+      assert.equal(cipher.decode(33, " "), " ");
     });
 });
-})
+});
